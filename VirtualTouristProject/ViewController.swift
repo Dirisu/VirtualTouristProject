233534ee78fd40cef61b1566c11e3762ad5b0d6d
Dirisu <dirisumarvellous94@gmail.com>
2022-06-09 14:19:50 +0100

    Initial Commit

---
+++ VirtualTouristProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTouristProject
+//
+//  Created by Marvellous Dirisu on 09/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
